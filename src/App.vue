48be94643302c814e5805192ac0264d6e88955c7
GustavoSirkis <gustavosirkis@gmail.com>
2022-12-21 12:38:46 -0300

    "Build"

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,11 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HelloWorld msg="Guz Portifolio" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Início</RouterLink>
+        <RouterLink to="/about">Sobre</RouterLink>
       </nav>
     </div>
   </header>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,9 +11,9 @@ defineProps({
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
     <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+      Bem vindo, convido você a conhecer minha jornada no mundo da programação.
+  <!--     <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. -->
     </h3>
   </div>
 </template>
